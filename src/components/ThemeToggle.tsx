8ecb9e3f72496739baba24bda397eba946db6672
Jack <jackbaude@gmail.com>
2020-08-21 14:12:26 -0500

    fixed dark and light mode toggle to remember state across site

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,24 +1,23 @@
-import React, {useState} from "react"
+import React, {memo, useEffect, useState} from "react"
 import Cookies from 'universal-cookie';
 
 const ThemeToggle = () => {
     const cookies = new Cookies();
-
-    const [themeState, setThemeState] = useState((cookies.get('theme')))
+    const [themeState, setThemeState] = useState(localStorage.getItem('mode'))
     const toggleTheme = () => {
-
-        const theme = themeState === 'light' ? 'dark' : 'light';
-        cookies.set('theme', theme, { path: '/' });
+        const theme = localStorage.getItem('mode') === 'dark' ? 'light' : 'dark';
         setThemeState(theme)
-        document.documentElement.setAttribute("data-theme", theme);
+        localStorage.setItem('mode', theme);
     }
+    document.documentElement.setAttribute("data-theme", localStorage.getItem('mode'));
+
     return (
         <div>
             <label className="switch">
-                <input type="checkbox" onClick={toggleTheme}/>
+                <input type="checkbox" onClick={toggleTheme} defaultChecked={((localStorage.getItem('mode')) === "dark")}/>
                 <span className="slider round"/>
             </label>
         </div>
     )
 }
-export default ThemeToggle
+export default memo(ThemeToggle)
