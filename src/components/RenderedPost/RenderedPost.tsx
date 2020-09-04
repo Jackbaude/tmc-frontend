e2ac1ff9dc43832a9ab52405fd5f397610049886
Jack <jackbaude@gmail.com>
2020-09-04 17:28:33 -0500

    Bug fix links werent working in the rendererd post
    Never added the link wrap code to the rendered post

--- src/components/RenderedPost/RenderedPost.tsx
+++ src/components/RenderedPost/RenderedPost.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useMemo, useCallback, useEffect} from 'react'
-import {Node, createEditor} from 'slate'
+import {Node, createEditor, Transforms, Range, Editor} from 'slate'
 import {
     Slate,
     Editable,
@@ -11,6 +11,7 @@ import {
 import {withHistory} from 'slate-history'
 import {css} from 'emotion'
 import {useParams} from "react-router";
+import isUrl from "is-url";
 
 const RenderedPost = () => {
     const {id} = useParams();
@@ -41,7 +42,7 @@ const RenderedPost = () => {
     const renderElement = useCallback(props => <Element {...props} />, [])
     const renderLeaf = useCallback(props => <Leaf {...props} />, [])
     const editor = useMemo(
-        () => withImages(withHistory(withReact(createEditor()))),
+        () => withImages(withLinks(withHistory(withReact(createEditor())))),
         []
     )
 
@@ -74,10 +75,37 @@ const RenderedPost = () => {
                 />
                 <div className={"spacing-block"}/>
             </Slate>
-            <a className={"btn btn-primary btn-lg"} href={"/edit-post/" + id}>Edit Post</a>
+            <a className={"btn btn-lg"} href={"/edit-post/" + id}>Edit Post</a>
         </div>
     )
 
+}
+const withLinks = editor => {
+    const {insertData, insertText, isInline} = editor
+
+    editor.isInline = element => {
+        return element.type === 'link' ? true : isInline(element)
+    }
+
+    editor.insertText = text => {
+        if (text && isUrl(text)) {
+            wrapLink(editor, text)
+        } else {
+            insertText(text)
+        }
+    }
+
+    editor.insertData = data => {
+        const text = data.getData('text/plain')
+
+        if (text && isUrl(text)) {
+            wrapLink(editor, text)
+        } else {
+            insertData(data)
+        }
+    }
+
+    return editor
 }
 const withImages = editor => {
     const {isVoid} = editor
@@ -107,7 +135,7 @@ const Element = props => {
             return <ImageElement {...props} />
         case 'link':
             return (
-                <a {...attributes} href={element.url}>
+                <a {...attributes} href={element.url} target={"_blank"}>
                     {children}
                 </a>
             )
@@ -153,6 +181,35 @@ const ImageElement = ({attributes, children, element}) => {
             {children}
         </div>
     )
+}
+const unwrapLink = editor => {
+    Transforms.unwrapNodes(editor, {match: n => n.type === 'link'})
+}
+
+const wrapLink = (editor, url) => {
+    if (isLinkActive(editor)) {
+        unwrapLink(editor)
+    }
+
+    const {selection} = editor
+    const isCollapsed = selection && Range.isCollapsed(selection)
+    const link = {
+        type: 'link',
+        url,
+        children: isCollapsed ? [{text: url}] : [],
+    }
+
+    if (isCollapsed) {
+        Transforms.insertNodes(editor, link)
+    } else {
+        Transforms.wrapNodes(editor, link, {split: true})
+        Transforms.collapse(editor, {edge: 'end'})
+    }
+}
+
+const isLinkActive = editor => {
+    const [link] = Editor.nodes(editor, {match: n => n.type === 'link'})
+    return !!link
 }
 const initialValue = [
     {
