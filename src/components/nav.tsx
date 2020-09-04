c347b19a0c07f27c1d171f695894d0b387361db8
Jack <jackbaude@gmail.com>
2020-09-04 17:22:46 -0500

    Added the donation link as well as removing the tmc text with a img

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,7 +2,8 @@ import React from "react"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faDiscord, faGithub} from '@fortawesome/free-brands-svg-icons'
 import ThemeToggle from "./ThemeToggle";
-
+import {faDollarSign} from "@fortawesome/free-solid-svg-icons";
+import book from "./Home/img/book.png"
 const Nav = () => {
     return (
         <div>
@@ -13,11 +14,11 @@ const Nav = () => {
                     <span className="navbar-toggler-icon"/>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarToggler">
-                    <a className="navbar-brand" href="/"><h2 className={"flip-in"}>Technical Minecraft Wiki</h2></a>
+                    <a className="navbar-brand" href="/"><h1 className={"jello"}><img src={book} alt={"book"} height={"50em"} width={"50em"}/></h1></a>
                     <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li className="nav-item active">
-                            <a className="nav-link link" href="/">Home<span className="sr-only">(current)</span></a>
-                        </li>
+                        {/*<li className="nav-item active">*/}
+                        {/*    <a className="nav-link link" href="/">Home<span className="sr-only">(current)</span></a>*/}
+                        {/*</li>*/}
                         <li className="nav-item">
                             <a className="nav-link link" href="/posts">Posts</a>
                         </li>
@@ -30,17 +31,28 @@ const Nav = () => {
                         <li className="nav-item">
                             <a className="nav-link link" href="/about">About</a>
                         </li>
+
+                    </ul>
+                    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
                         <li className="nav-item">
                             <a className="nav-link" href="https://github.com/Jackbaude/tmc-wiki" target="_blank"
-                               rel="noreferrer noopener"><FontAwesomeIcon icon={faGithub} size={"lg"}/></a>
+                               rel="noreferrer noopener"><FontAwesomeIcon icon={faGithub} size={"2x"}/></a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="https://discord.gg/FcTFg2E" target="_blank"
-                               rel="noreferrer noopener"><FontAwesomeIcon icon={faDiscord} size={"lg"}/></a>
+                               rel="noreferrer noopener"><FontAwesomeIcon icon={faDiscord} size={"2x"}/></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="https://streamelements.com/jjakuu/tip"><FontAwesomeIcon icon={faDollarSign} size={"2x"}/></a>
                         </li>
 
+                        <li>
+                            <ThemeToggle/>
+                        </li>
                     </ul>
-                    <ThemeToggle/>
+
+
+
                 </div>
             </nav>
         </div>
